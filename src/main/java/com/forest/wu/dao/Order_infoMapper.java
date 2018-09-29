1153b9e56b743c8965d16b14c72edc95bd6bc8f8
Forest7677 <854738796@qq.com>
2018-09-29 09:59:50 +0800

    jsp目录

--- src/main/java/com/forest/wu/dao/Order_infoMapper.java
+++ src/main/java/com/forest/wu/dao/Order_infoMapper.java
@@ -2,6 +2,8 @@ package com.forest.wu.dao;
 
 import com.forest.wu.pojo.Order_info;
 
+import java.util.List;
+
 public interface Order_infoMapper {
     int deleteByPrimaryKey(Integer id);
 
@@ -14,4 +16,15 @@ public interface Order_infoMapper {
     int updateByPrimaryKeySelective(Order_info record);
 
     int updateByPrimaryKey(Order_info record);
+
+    /**
+    * @author: 肖林辉
+    * @Description   根据查询条件查询出对应的订单
+    * @Date: 9:20 2018/9/29/029
+    * @Param：[order_info]
+    * @return：java.util.List<com.forest.wu.pojo.Order_info>
+    **/
+    List<Order_info> selectOrderByOption(Order_info order);
+
+
 }
