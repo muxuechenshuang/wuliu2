7d5681da567c734a7d172ba03fbe8d0d57158ac7
你是一只大羊驼呀 <for-alpaca@qq.com>
2018-10-11 09:47:00 +0800

    站内信息初步完成,附加未完成的支付宝支付

--- src/main/java/com/forest/wu/dao/NoteMapper.java
+++ src/main/java/com/forest/wu/dao/NoteMapper.java
@@ -1,6 +1,9 @@
 package com.forest.wu.dao;
 
 import com.forest.wu.pojo.Note;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface NoteMapper {
     int deleteByPrimaryKey(Long id);
@@ -14,4 +17,14 @@ public interface NoteMapper {
     int updateByPrimaryKeySelective(Note record);
 
     int updateByPrimaryKey(Note record);
+
+    /**
+     *
+     * @author: 任一
+     * @Description 根据用户Id查询五条时间最近的信息
+     * @Date: 17:18 2018/10/10
+     * @Param：
+     * @return：
+     */
+    List<Note> getNoteSelf(@Param(value = "clientId") Integer clientId);
 }
