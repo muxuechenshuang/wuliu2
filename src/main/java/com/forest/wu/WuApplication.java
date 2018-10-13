d708c5ad5b048505e2a1ef4818c332a3f19c7942
Forest7677 <854738796@qq.com>
2018-10-13 09:24:58 +0800

    10-13

--- src/main/java/com/forest/wu/WuApplication.java
+++ src/main/java/com/forest/wu/WuApplication.java
@@ -17,28 +17,4 @@ public class WuApplication {
     public static void main(String[] args) {
         SpringApplication.run(WuApplication.class, args);
     }
-
-    /**
-     *
-     * @author: 任一
-     * @Description
-     * @Date: 9:58 2018/9/30
-     * JSON配置(fastjson)
-     */
-
-    @Bean
-    public HttpMessageConverters fastJsonHttpMessageConverters() {
-        FastJsonHttpMessageConverter converter = new FastJsonHttpMessageConverter();
-        FastJsonConfig fastJsonConfig = new FastJsonConfig();
-        fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
-
-        //日期格式化
-        fastJsonConfig.setDateFormat("yyyy-MM-dd HH:mm:ss");
-        converter.setFastJsonConfig(fastJsonConfig);
-        return new HttpMessageConverters(converter);
-    }
-
-
-
-
 }
